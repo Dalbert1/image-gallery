4acf2e80cdbea06190eb4f642b5d78c634a52f32
Dylan Albert <dylan.m.albert1@gmail.com>
2020-06-07 02:22:02 +0000

    Update 6-6-2020

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -13,7 +13,7 @@ def get_password():
 	result = f.readline()
 	f.close()
 	return result[:-1] #drops new line char at end of result
-	
+
 def connect():
 	global connection #declare as global
 	connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
@@ -26,12 +26,40 @@ def execute(query,args=None):
 	else:
 		cursor.execute(query, args)
 	return cursor
+
+def listUsers():
+	res = execute('select * from users;')
+	return res
+		
+def insertUser(new_account):
+	res = execute("insert into users (username, password, full_name) values (%s, %s, %s);", (new_account[0], new_account[1], new_account[2]));
+	connection.commit()
+
+	
+def editUser(userToEdit):
+	username = userToEdit[0]
+	password = userToEdit[1]
+	full_name = userToEdit[2]
+	if (password != ''):
+		res = execute("update users set password = %s where username=%s;", (password, username))
+	if (full_name != ''):
+		res = execute("update users set full_name = %s where username=%s;", (full_name, username))
+	connection.commit()	
+	
+def checkExists(username):
+	res = execute('select * from users where username=%s;', (username,))
+	row = res.fetchone()
+	if (row):
+		return True
+	return False
+
+def deleteUser(unluckyUser):
+	res = execute("delete from users where username = %s;", (unluckyUser,))
+	connection.commit()
 	
 def main():
 	connect()
-	res = execute('select * from users;')
-	for row in res:
-		print(row)
-		
+	connection.close()
+
 if __name__ == '__main__':
 	main()
