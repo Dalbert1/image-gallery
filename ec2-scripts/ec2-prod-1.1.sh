3e5b5a4e67cb19d9dbf7a8f8ecdaef084e39c670
EC2 Default User <ec2-user@ip-10-0-1-97.us-east-2.compute.internal>
2020-06-17 03:17:47 +0000

    Modified scripts

---
+++ ec2-scripts/ec2-prod-1.1.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.1"
+BUCKET="edu.au.cc.python-image-gallery-config"
+
+# Install Packages
+sudo yum update -y
+sudo yum install -y python3 git
+amazon-linux-extras install -y nginx1
+
+# Configure/install custom software
+cd /home/ec2-user
+git clone https://github.com/Dalbert1/python-image-gallery.git
+chown -R ec2-user:ec2-user python-image-gallery
+cd python-image-gallery/
+pip3 install -r requirements.txt --user
+
+aws s3 cp s3://${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx
+aws s3 cp s3://${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d
+
+
+# Start/enable services
+systemctl stop postfix
+systemctl disable postfix
+
+su ec2-user -l -c "cd ~/python-image-gallery && ./start">/var/log/image_gallery.log 2>&1 &
